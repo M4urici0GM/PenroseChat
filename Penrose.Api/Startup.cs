2a6504cc747f2f9455d6f0650c482f57ad091418
mauricio barbosa <mgbftw@gmail.com>
2021-08-02 22:18:55 -0300

    Dont remember

--- Penrose.Api/Startup.cs
+++ Penrose.Api/Startup.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Penrose.Application.Extensions;
 using Penrose.Microservices.User.Extensions;
 using Penrose.Microservices.User.Middlewares;
 using Penrose.Persistence;
